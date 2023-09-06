4d98f3783ff2a16436cd274f5caa5c002db77e31
Fayisa Yahiya <fayivv@gmail.com>
2023-09-06 22:25:13 +0400

    module sample updated in feature branch

--- moduleSample.py
+++ moduleSample.py
@@ -1,6 +1,7 @@
-def greater(num1,num2):
-    if(num1>num2) :
-        print(str(num1)+" is greater than "+str(num2))
-    elif(num1<num2) :
-        print(str(num2) + " is greater than " + str(num1))
-greater(10,15)
+def sum() :
+    num1=input("Enter the first number:")
+    num2=input("Enter the second number:")
+    sum=int(num1)+int(num2)
+    print("Sum="+str(sum))
+
+sum()
