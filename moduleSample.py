4572b658bbdafcb9fd0fe6f948d29a8be5b17a22
Fayisa Yahiya <fayivv@gmail.com>
2023-09-06 15:56:27 +0400

    first code is written on moduleSample.py

---
+++ moduleSample.py
@@ -0,0 +1,6 @@
+def greater(num1,num2):
+    if(num1>num2) :
+        print(str(num1)+" is greater than "+str(num2))
+    elif(num1<num2) :
+        print(str(num2) + " is greater than " + str(num1))
+greater(10,15)
