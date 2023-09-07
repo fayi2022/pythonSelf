ca38e3e09c45ff24ff13fde934ead6934828f355
Fayisa Yahiya <fayivv@gmail.com>
2023-09-07 10:41:30 +0400

    Checking the condition for division is added

--- moduloDiv.py
+++ moduloDiv.py
@@ -1,4 +1,7 @@
 x=int(input("Enter the first num:"))
 y=int(input("Enter the second number:"))
-z=x%y
-print("The remainder is:"+str(z))
+if(y!=0) :
+    z=x%y
+    print("The remainder is:"+str(z))
+else :
+    print("Division is not defined")
